9f79f7321fa62f7449ca3b4269bd4967b7bef258
Remaerdeno <remaerdeno@gmail.com>
2017-10-19 20:25:53 +0800

    Update sort.py
    八大排序算法

--- sort.py
+++ sort.py
@@ -60,3 +60,74 @@ def quick_sort(lists,left,right):
     quick_sort(lists,left+1,high)
     return lists
 print(quick_sort(list,0,len(list)-1))
+#直接选择排序
+def select_sort(lists):
+    # 选择排序
+    count = len(lists)
+    for i in range(0, count):
+        min = i
+        for j in range(i + 1, count):
+            if lists[min] > lists[j]:
+                min = j
+        lists[min], lists[i] = lists[i], lists[min]
+    return lists
+#堆排序
+def adjust_heap(lists, i, size):
+    lchild = 2 * i + 1
+    rchild = 2 * i + 2
+    max = i
+    if i < size / 2:
+        if lchild < size and lists[lchild] > lists[max]:
+            max = lchild
+        if rchild < size and lists[rchild] > lists[max]:
+            max = rchild
+        if max != i:
+            lists[max], lists[i] = lists[i], lists[max]
+            adjust_heap(lists, max, size)
+ 
+def build_heap(lists, size):
+    for i in range(0, (size/2))[::-1]:
+        adjust_heap(lists, i, size)
+ 
+def heap_sort(lists):
+    size = len(lists)
+    build_heap(lists, size)
+    for i in range(0, size)[::-1]:
+        lists[0], lists[i] = lists[i], lists[0]
+        adjust_heap(lists, 0, i)
+#归并排序
+def merge(left, right):
+    i, j = 0, 0
+    result = []
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    result += left[i:]
+    result += right[j:]
+    return result
+ 
+def merge_sort(lists):
+    # 归并排序
+    if len(lists) <= 1:
+        return lists
+    num = len(lists) / 2
+    left = merge_sort(lists[:num])
+    right = merge_sort(lists[num:])
+    return merge(left, right)
+#基数排序
+import math
+def radix_sort(lists, radix=10):
+    k = int(math.ceil(math.log(max(lists), radix)))
+    bucket = [[] for i in range(radix)]
+    for i in range(1, k+1):
+        for j in lists:
+            bucket[j/(radix**(i-1)) % (radix**i)].append(j)
+        del lists[:]
+        for z in bucket:
+            lists += z
+            del z[:]
+    return lists
